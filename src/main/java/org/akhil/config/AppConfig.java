333d550b10a8d31f104021f509d8735379714bf0
akhilmw <akhilmw2@gmail.com>
2025-09-23 23:12:10 -0500

    singleton for appCOnfig

--- src/main/java/org/akhil/config/AppConfig.java
+++ src/main/java/org/akhil/config/AppConfig.java
@@ -1,2 +1,43 @@
-package org.akhil.config;public class AppConfig {
+package org.akhil.config;
+
+import org.akhil.domain.Region;
+
+/**
+ * Uses Singleton - DCL
+ */
+public class AppConfig {
+    private static volatile AppConfig appConfig;
+    private final Region region;
+    private final String notifyType;
+    private final String exporter;
+
+    // TODO : dont hardcode use JSON loading
+    private AppConfig () {
+        this.region = Region.EU;
+        this.notifyType = "Email";
+        this.exporter = "shopify";
+    }
+
+    public static AppConfig getInstance() {
+        if (appConfig == null) {
+            synchronized (AppConfig.class) {
+                if (appConfig == null) {
+                    appConfig = new AppConfig();
+                }
+            }
+        }
+        return  appConfig;
+    }
+
+    public Region getRegion() {
+        return this.region;
+    }
+
+    public String getNotifyType() {
+        return this.notifyType;
+    }
+
+    public String getExporter() {
+        return this.exporter;
+    }
 }
